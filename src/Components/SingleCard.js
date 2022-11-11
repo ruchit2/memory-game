c5b0552fe439d4e0900c3f8e3a7c3cc9cb4c6620
Ruchit Bhandari <ruchitbhandari1@gmail.com>
2022-11-12 04:48:35 +0530

    Project checkpoint

---
+++ src/Components/SingleCard.js
@@ -0,0 +1,26 @@
+import './SingleCard.css';
+
+const SingleCard = function ({card, handleChoice, flipped, disabled}) {
+
+    const handleClick = function () {
+      if(!disabled){
+        handleChoice(card)
+      }
+    }
+
+    return (
+      <div className="card">
+        <div className={flipped ? 'flipped' : ''}>
+          <img className="front" src={card.src} alt="card front" />
+          <img 
+          onClick={handleClick} 
+          className="back" 
+          src="/img/cover.png" 
+          alt="card back" 
+          />
+        </div>
+      </div>
+    );
+}
+
+export default SingleCard
