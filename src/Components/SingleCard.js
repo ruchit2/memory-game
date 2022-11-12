5da1b0381a1f7aec8e8f392673e6facb422a317d
Ruchit Bhandari <ruchitbhandari1@gmail.com>
2022-11-12 05:31:44 +0530

    Added build file to github pages

--- src/Components/SingleCard.js
+++ src/Components/SingleCard.js
@@ -1,26 +1,25 @@
-import './SingleCard.css';
+import "./SingleCard.css";
 
-const SingleCard = function ({card, handleChoice, flipped, disabled}) {
-
-    const handleClick = function () {
-      if(!disabled){
-        handleChoice(card)
-      }
+const SingleCard = function ({ card, handleChoice, flipped, disabled }) {
+  const handleClick = function () {
+    if (!disabled) {
+      handleChoice(card);
     }
+  };
 
-    return (
-      <div className="card">
-        <div className={flipped ? 'flipped' : ''}>
-          <img className="front" src={card.src} alt="card front" />
-          <img 
-          onClick={handleClick} 
-          className="back" 
-          src="/img/cover.png" 
-          alt="card back" 
-          />
-        </div>
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="card front" />
+        <img
+          onClick={handleClick}
+          className="back"
+          src="./img/cover.png"
+          alt="card back"
+        />
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default SingleCard
+export default SingleCard;
